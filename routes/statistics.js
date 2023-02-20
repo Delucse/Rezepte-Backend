0b070ea0d884d0bfbc1007b2b7de304fcf5cbd89
Delucse <46593742+Delucse@users.noreply.github.com>
2023-02-20 20:28:29 +0100

    further statistics

--- routes/statistics.js
+++ routes/statistics.js
@@ -4,30 +4,151 @@ var api = express.Router();
 const User = require('../models/user');
 const Picture = require('../models/picture');
 const Recipe = require('../models/recipe');
+const RecipeUser = require('../models/recipeUser');
 
-api.get('/', async function (req, res, next) {
-    const aggregation = [
+const countAggregation = [
+    {
+        $group: {
+            _id: {
+                month: { $month: '$createdAt' },
+                year: { $year: '$createdAt' },
+            },
+            count: { $sum: 1 },
+        },
+    },
+    {
+        $sort: {
+            '_id.year': 1,
+            '_id.month': 1,
+        },
+    },
+    {
+        $project: {
+            _id: 0,
+            month: '$_id.month',
+            year: '$_id.year',
+            count: 1,
+        },
+    },
+];
+
+const mostAggregation = (match) => {
+    var aggregation = [];
+    if (match) {
+        aggregation.push({
+            $match: match,
+        });
+    }
+    aggregation = aggregation.concat([
         {
             $group: {
-                _id: {
-                    month: { $month: '$createdAt' },
-                    year: { $year: '$createdAt' },
+                _id: '$user',
+                count: {
+                    $sum: 1,
                 },
-                count: { $sum: 1 },
             },
         },
         {
             $sort: {
-                '_id.year': 1,
-                '_id.month': 1,
+                count: -1,
+                _id: 1,
             },
         },
-    ];
-    const userCount = await User.aggregate(aggregation);
-    const recipeCount = await Recipe.aggregate(aggregation);
-    const imageCount = await Picture.aggregate(aggregation);
+        {
+            $limit: 1,
+        },
+        {
+            $lookup: {
+                from: 'users',
+                localField: '_id',
+                foreignField: '_id',
+                as: 'user',
+            },
+        },
+        {
+            $project: {
+                _id: 0,
+                name: {
+                    $arrayElemAt: ['$user.username', 0],
+                },
+                count: 1,
+            },
+        },
+    ]);
+    return aggregation;
+};
 
-    res.json({ users: userCount, recipes: recipeCount, images: imageCount });
+api.get('/', async function (req, res, next) {
+    const userCount = await User.aggregate(countAggregation);
+    const recipeCount = await Recipe.aggregate(countAggregation);
+    const mostRecipes = await Recipe.aggregate(mostAggregation());
+    const imageCount = await Picture.aggregate(countAggregation);
+    const mostImages = await Picture.aggregate(mostAggregation());
+    const totalFavorites = await RecipeUser.aggregate([
+        {
+            $match: {
+                favorite: true,
+            },
+        },
+        {
+            $count: 'count',
+        },
+    ]);
+    const mostFavorites = await RecipeUser.aggregate(
+        mostAggregation({ favorite: true })
+    );
+    const favoriteRecipe = await RecipeUser.aggregate([
+        {
+            $match: {
+                favorite: true,
+            },
+        },
+        {
+            $group: {
+                _id: '$recipe',
+                count: {
+                    $sum: 1,
+                },
+            },
+        },
+        {
+            $sort: {
+                count: -1,
+                _id: 1,
+            },
+        },
+        {
+            $limit: 1,
+        },
+        {
+            $lookup: {
+                from: 'recipes',
+                localField: '_id',
+                foreignField: '_id',
+                as: 'recipe',
+            },
+        },
+        {
+            $project: {
+                _id: { $arrayElemAt: ['$recipe._id', 0] },
+                title: {
+                    $arrayElemAt: ['$recipe.title', 0],
+                },
+                count: 1,
+            },
+        },
+    ]);
+
+    res.json({
+        users: { count: userCount },
+        recipes: { count: recipeCount, user: mostRecipes[0] },
+        images: { count: imageCount, user: mostImages[0] },
+        favorites: {
+            user: mostFavorites[0],
+            recipe: favoriteRecipe[0],
+            total: totalFavorites[0].count,
+        },
+    });
 });
 
 module.exports = api;
