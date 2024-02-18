48154aad5bac75bb4a0fd767c9413d6e38a4bcd7
Delucse <46593742+Delucse@users.noreply.github.com>
2024-02-18 13:08:41 +0100

    http-proxy-middleware

--- api.js
+++ api.js
@@ -6,13 +6,14 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var cors = require('cors');
-
-var api = express();
+var { createProxyMiddleware } = require('http-proxy-middleware');
 
 //reads in configuration from a .env file
 require('dotenv').config({
     path: `.env.${process.env.NODE_ENV || 'development'}`,
 });
+
+var api = express();
 
 api.use(logger('dev'));
 api.use(cors({ credentials: true, origin: process.env.APP_BASE_URL }));
@@ -23,77 +24,90 @@ api.set('view engine', 'hbs');
 api.use(bodyParser.json({ limit: '10mb', extended: true }));
 api.use(cookieParser());
 
-if (process.env.MEDIA) {
-    if (!process.env.IMAGEKIT_PUBLIC_KEY || process.env.MEDIA_PATH) {
-        api.use(
-            `/${process.env.NODE_ENV === 'production' ? '' : 'media'}`,
-            express.static(
-                path.join(__dirname, process.env.MEDIA_PATH || 'public')
-            )
+// proxy
+const mediaProxy = createProxyMiddleware(
+    function (pathname, req) {
+        console.log(
+            req.method === 'GET' && req.headers.host === process.env.MEDIA_HOST
         );
-    } else {
-        var mediaRouter = express.Router();
-        mediaRouter.get('/:media', (req, res) => {
-            request(
-                `https://ik.imagekit.io/${process.env.IMAGEKIT_ID}/` +
-                    req.params.media
-            ).pipe(res);
-        });
-        api.use(
-            `/${process.env.NODE_ENV === 'production' ? '' : 'media'}`,
-            mediaRouter
+        return (
+            req.method === 'GET' && req.headers.host === process.env.MEDIA_HOST
         );
+    },
+    {
+        target: `${process.env.API_BASE_URL}/media`,
     }
-} else if (process.env.SHARE) {
-    var shareRouter = express.Router();
-    shareRouter.get('/', (req, res) => {
-        res.redirect(process.env.APP_BASE_URL);
-    });
-    var sRouter = require('./routes/share');
-    shareRouter.use('/r', sRouter);
+);
+const shareProxy = createProxyMiddleware(
+    function (pathname, req) {
+        return (
+            req.method === 'GET' && req.headers.host === process.env.SHARE_HOST
+        );
+    },
+    {
+        target: `${process.env.API_BASE_URL}/share`,
+    }
+);
+api.use(mediaProxy);
+api.use(shareProxy);
+
+// media
+if (!process.env.IMAGEKIT_PUBLIC_KEY || process.env.MEDIA_PATH) {
     api.use(
-        `/${process.env.NODE_ENV === 'production' ? '' : 'share'}`,
-        shareRouter
+        '/media',
+        express.static(path.join(__dirname, process.env.MEDIA_PATH || 'public'))
     );
 } else {
-    var apiRouter = express.Router();
-
-    var indexRouter = require('./routes/index');
-    apiRouter.use('/', indexRouter);
-
-    var statisticRouter = require('./routes/statistics');
-    apiRouter.use('/stats', statisticRouter);
-
-    var authRouter = require('./routes/auth');
-    apiRouter.use('/auth', authRouter);
-
-    var userRouter = require('./routes/user');
-    apiRouter.use('/user', userRouter);
-
-    var imageRouter = require('./routes/image');
-    apiRouter.use('/recipe/image', imageRouter);
-
-    var recipeRouter = require('./routes/recipe');
-    apiRouter.use('/recipe', recipeRouter);
-
-    var favoriteRouter = require('./routes/favorite');
-    apiRouter.use('/recipe/favorite', favoriteRouter);
-
-    var noteRouter = require('./routes/note');
-    apiRouter.use('/recipe/note', noteRouter);
-
-    var mailRouter = require('./routes/mail');
-    apiRouter.use('/mail', mailRouter);
-
-    api.use(
-        `/${
-            process.env.MONGODB_URI || process.env.NODE_ENV === 'production'
-                ? ''
-                : 'api'
-        }`,
-        apiRouter
-    );
+    var mediaRouter = express.Router();
+    mediaRouter.get('/:media', (req, res) => {
+        request(
+            `https://ik.imagekit.io/${process.env.IMAGEKIT_ID}/` +
+                req.params.media
+        ).pipe(res);
+    });
+    api.use('/media', mediaRouter);
 }
+
+// share
+var shareRouter = express.Router();
+shareRouter.get('/', (req, res) => {
+    res.redirect(process.env.APP_BASE_URL);
+});
+var sRouter = require('./routes/share');
+shareRouter.use('/r', sRouter);
+api.use('/share', shareRouter);
+
+// api
+var apiRouter = express.Router();
+
+var indexRouter = require('./routes/index');
+apiRouter.use('/', indexRouter);
+
+var statisticRouter = require('./routes/statistics');
+apiRouter.use('/stats', statisticRouter);
+
+var authRouter = require('./routes/auth');
+apiRouter.use('/auth', authRouter);
+
+var userRouter = require('./routes/user');
+apiRouter.use('/user', userRouter);
+
+var imageRouter = require('./routes/image');
+apiRouter.use('/recipe/image', imageRouter);
+
+var recipeRouter = require('./routes/recipe');
+apiRouter.use('/recipe', recipeRouter);
+
+var favoriteRouter = require('./routes/favorite');
+apiRouter.use('/recipe/favorite', favoriteRouter);
+
+var noteRouter = require('./routes/note');
+apiRouter.use('/recipe/note', noteRouter);
+
+var mailRouter = require('./routes/mail');
+apiRouter.use('/mail', mailRouter);
+
+api.use('/', apiRouter);
 
 // catch 404 and forward to error handler
 api.use(function (req, res, next) {
